1ac431a993509086431b063cb8be74a08ec4b7a7
Iman Ghasemi Arani <iman.ghasemi.arani@gmail.com>
2023-02-11 16:25:07 +0330

    update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,10 @@
-echo on
+@REM echo on
 call flutter clean
 call flutter build web --web-renderer html
 call xcopy /s /y /r build\web D:\Dev_Files\GitHub\imanghasemiarani.github.io\.
 call cd D:\Dev_Files\GitHub\imanghasemiarani.github.io
+call d:
 call git add .
 call git commit -m "update"
 call git push
-pause
+@REM pause
