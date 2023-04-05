c2feace55d2007701cc75bc5e7ee87f568bcd1d0
Iman Ghasemi Arani <iman.ghasemi.arani@gmail.com>
2023-04-06 02:13:04 +0330

    add projects to work page

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,59 @@ import 'package:flutter/material.dart';
 double? toolbarHeight;
 
 final ValueNotifier<bool> isVisibleDancingArrow = ValueNotifier<bool>(true);
+
+const List<Map<String, dynamic>> projectsData = [
+  {
+    'iconUrl':
+        'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+    'title': 'Personal Website (Flutter Web)',
+    'description':
+        'This website is built using Flutter Web. It is a responsive website and is hosted on Firebase.',
+    'imgUrls': null,
+    'rLink': '',
+    'dLink': null,
+  },
+  {
+    'iconUrl':
+        'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+    'title': 'Brain:Code (Flutter Mobile)',
+    'description':
+        'This is a mobile application built using Flutter. It is a responsive website and is hosted on Firebase.',
+    'imgUrls': [
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+    ],
+    'rLink': '',
+    'dLink': '',
+  },
+  {
+    'iconUrl':
+        'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+    'title': 'Dakke (Flutter Mobile)',
+    'description': 'This is a mobile application built using Flutter.',
+    'imgUrls': [
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+    ],
+    'rLink': '',
+    'dLink': '',
+  },
+  {
+    'iconUrl': null,
+    'title': 'IG Music (Flutter Mobile)',
+    'description': 'This is a mobile application built using Flutter.',
+    'imgUrls': [
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+      'https://d.newsweek.com/en/full/2151074/stock-image-shocked-cat.webp?w=790&f=8d422d28be66a0132a90da7ac2d23e69',
+    ],
+    'rLink': '',
+    'dLink': '',
+  },
+];
--- lib/pages/work.dart
+++ lib/pages/work.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gallery_image_viewer/gallery_image_viewer.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
+import '../constant.dart';
 import '../layouts/adaptive.dart';
 import '../widgets/title_widget.dart';
 import 'animated_cursor_position.dart';
@@ -16,11 +18,234 @@ class WorkPage extends StatelessWidget {
           children: const [
             TitleWidget('Works'),
             _WorkExp(),
-            SizedBox(height: 100)
+            SizedBox(height: 100),
+            _Projects(),
+            SizedBox(height: 100),
           ],
         ),
       ),
     );
+  }
+}
+
+class _Projects extends StatelessWidget {
+  const _Projects();
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 900),
+      child: Column(
+        children: [
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Projects',
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      fontSize:
+                          Theme.of(context).textTheme.titleLarge!.fontSize! +
+                              5))),
+          const SizedBox(height: 40),
+          ...projectsData.map((e) {
+            if (isDisplayDesktop(context)) {
+              return _DesktopProjectItem(
+                e['iconUrl'],
+                e['title'],
+                e['description'],
+                e['rLink'],
+                e['dLink'],
+                e['imgUrls'],
+              );
+            } else {
+              return _MobileProjectItem(
+                e['iconUrl'],
+                e['title'],
+                e['description'],
+                e['rLink'],
+                e['dLink'],
+                e['imgUrls'],
+              );
+            }
+          }),
+        ],
+      ),
+    );
+  }
+}
+
+class _MobileProjectItem extends StatelessWidget {
+  const _MobileProjectItem(
+    this.iconUrl,
+    this.title,
+    this.description,
+    this.rLink,
+    this.dLink,
+    this.imgUrls,
+  );
+
+  final String? iconUrl;
+  final String title;
+  final String description;
+  final String? rLink;
+  final String? dLink;
+  final List<String>? imgUrls;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: SizedBox(
+            height: imgUrls == null ? 350 : 350 * 2,
+            child: Center(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                  Row(children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: iconUrl != null
+                          ? Image.network(iconUrl!,
+                              width: 75, height: 75, fit: BoxFit.cover)
+                          : const SizedBox.square(
+                              dimension: 75, child: Icon(Icons.circle_rounded)),
+                    ),
+                    const SizedBox(width: 20),
+                    Text(title,
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleLarge
+                            ?.copyWith(color: Colors.blue)),
+                  ]),
+                  const SizedBox(height: 10),
+                  Text(description,
+                      style: Theme.of(context).textTheme.bodyMedium),
+                  const Spacer(),
+                  Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    if (dLink != null)
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: const Text('Download'),
+                      ),
+                    if (dLink != null && dLink != null)
+                      const SizedBox(width: 10),
+                    if (rLink != null)
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: const Text('Go to Repo'),
+                      ),
+                  ]),
+                  if (imgUrls != null) const SizedBox(height: 20),
+                  if (imgUrls != null)
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      AnimatedCursorMouseRegion(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Theme.of(context).colorScheme.primary),
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .primary
+                                  .withOpacity(0.1)),
+                          child: GalleryImageView(
+                              listImage:
+                                  imgUrls!.map((e) => NetworkImage(e)).toList(),
+                              width: 350,
+                              height: 350,
+                              imageDecoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey)))),
+                    ]),
+                ]))));
+  }
+}
+
+class _DesktopProjectItem extends StatelessWidget {
+  const _DesktopProjectItem(
+    this.iconUrl,
+    this.title,
+    this.description,
+    this.rLink,
+    this.dLink,
+    this.imgUrls,
+  );
+
+  final String? iconUrl;
+  final String title;
+  final String description;
+  final String? rLink;
+  final String? dLink;
+  final List<String>? imgUrls;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: SizedBox(
+            height: imgUrls == null ? 350 : 350,
+            child: Center(
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                  Column(children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: iconUrl != null
+                          ? Image.network(iconUrl!,
+                              width: 75, height: 75, fit: BoxFit.cover)
+                          : const SizedBox.square(
+                              dimension: 75, child: Icon(Icons.circle_rounded)),
+                    ),
+                  ]),
+                  const SizedBox(width: 20),
+                  Expanded(
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                        Text(title,
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge
+                                ?.copyWith(color: Colors.blue)),
+                        const SizedBox(height: 10),
+                        Text(
+                          description,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        const Spacer(),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              if (dLink != null)
+                                OutlinedButton(
+                                  onPressed: () {},
+                                  child: const Text('Download'),
+                                ),
+                              if (dLink != null && dLink != null)
+                                const SizedBox(width: 10),
+                              if (rLink != null)
+                                OutlinedButton(
+                                  onPressed: () {},
+                                  child: const Text('Go to Repo'),
+                                ),
+                            ]),
+                      ])),
+                  if (imgUrls != null) const SizedBox(width: 20),
+                  if (imgUrls != null)
+                    AnimatedCursorMouseRegion(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Theme.of(context).colorScheme.primary),
+                            color: Theme.of(context)
+                                .colorScheme
+                                .primary
+                                .withOpacity(0.1)),
+                        child: GalleryImageView(
+                            listImage:
+                                imgUrls!.map((e) => NetworkImage(e)).toList(),
+                            width: 350,
+                            height: 350,
+                            imageDecoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey)))),
+                ]))));
   }
 }
 
